dc639bf6d2927ae79ae0a54264ae03e8b93ce649
prabh <Horseprabhat625@gmail.com>
2020-07-18 16:52:38 +0530

    Just started initial commit

---
+++ LogExtractor.java
@@ -0,0 +1,108 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Main implements Runnable{
+    FastScanner sc;
+    PrintWriter pw;
+    final class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastScanner() {
+            try {
+                br = new BufferedReader(new InputStreamReader(System.in));
+                st = new StringTokenizer(br.readLine());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+ 
+        public long nlo() {
+            return Long.parseLong(next());
+        }
+ 
+        public String next() {
+            if (st.hasMoreTokens()) return st.nextToken();
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+ 
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+ 
+        public String nli() {
+            String line = "";
+            if (st.hasMoreTokens()) line = st.nextToken();
+            else try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (st.hasMoreTokens()) line += " " + st.nextToken();
+            return line;
+        }
+ 
+        public double nd() {
+            return Double.parseDouble(next());
+        }
+    }
+    public static void main(String[] args) throws Exception
+    {
+        new Thread(null,new Main(),"codeforces",1<<28).start();
+    }
+    public void run()
+    {
+        sc=new FastScanner();
+        pw=new PrintWriter(System.out);
+        solve();
+        pw.flush();
+        pw.close();
+    }
+    public long gcd(long a,long b)
+    {
+        return b==0L?a:gcd(b,a%b);
+    }
+    public long ppow(long a,long b,long mod)
+    {
+        if(b==0L)
+        return 1L;
+        long tmp=1;
+        while(b>1L)
+        {
+            if((b&1L)==1)
+            tmp*=a;
+            a*=a;
+            a%=mod;
+            tmp%=mod;
+            b>>=1;
+        }
+        return (tmp*a)%mod;
+    }
+    public  int gcd(int x,int y)
+    {
+        return y==0?x:gcd(y,x%y);
+    }
+    //////////////////////////////////
+    /////////////  LOGIC  ///////////
+    ////////////////////////////////
+    public void solve() {
+        int t=sc.ni();
+        while(t-->0)
+        {
+           int a=sc.ni();
+           int b=sc.ni();
+           int x=Math.min(a/3,b/3);
+           a-=x*3;b-=x*3;
+           x=x*2;
+           if(a+b>=3)
+           x++;
+           pw.println(x);
+            
+        }
+    }
+}
